47a7c1ecb06b4802d8733000493c51b72f84225f
NiviDevs <nevedya.gwalani@gmail.com>
2025-06-12 17:30:47 +0530

    added ability to leave teams with constraint validation (2 to 4 members)

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -59,11 +59,12 @@ export const joinTeam = async (req, res) => {
     if (!team) {
       return res.status(404).json({ error: "Team not found" });
     }
-
+    if (team.members.length >= 4) {
+      return res.status(400).json({ error: "Team is full. (max 4 members)" });
+    }
 
     team.members.push(user._id);
     await team.save();
-
 
     user.teamId = team._id;
     await user.save();
@@ -72,5 +73,48 @@ export const joinTeam = async (req, res) => {
   } catch (err) {
     console.error("Join team error:", err.message);
     res.status(500).json({ error: "Failed to join team" });
+  }
+};
+
+export const leaveTeam = async (req, res) => {
+  const { regId } = req.body;
+
+  try {
+    const user = await User.findOne({ regId: regId.toUpperCase() });
+
+    if (!user || !user.teamId) {
+      return res.status(400).json({ error: "User not in a team" });
+    }
+
+    const team = await Team.findById(user.teamId);
+
+    if (!team) {
+      return res.status(404).json({ error: "Team not found" });
+    }
+
+    if (team.members.length <= 2) {
+      return res
+        .status(400)
+        .json({ error: "A team must have at least 2 members" });
+    }
+
+    if (user.isLeader) {
+      return res.status(400).json({ error: "Leader cannot leave." });
+    }
+
+    // Remove user from team
+    team.members = team.members.filter(
+      (memberId) => memberId.toString() !== user._id.toString()
+    );
+    await team.save();
+
+    // Clear user's team info
+    user.teamId = null;
+    await user.save();
+
+    res.status(200).json({ message: "Successfully left the team" });
+  } catch (err) {
+    console.error("Leave team error:", err.message);
+    res.status(500).json({ error: "Failed to leave team" });
   }
 };
--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -1,8 +1,9 @@
-import express from 'express'
-import { createTeam, joinTeam } from '../controllers/teamController.js';
+import express from "express";
+import { createTeam, joinTeam , leaveTeam} from "../controllers/teamController.js";
 
 const router = express.Router();
-router.post('/create',createTeam);
-router.post('/join',joinTeam);
+router.post("/create", createTeam);
+router.post("/join", joinTeam);
+router.post("/leave", leaveTeam);
 
 export default router;
