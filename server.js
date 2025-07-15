3daf3c5ce49952fd96997172a8ece3ff5a48fd4f
NiviDevs <nevedya.gwalani@gmail.com>
2025-07-15 23:09:34 +0530

    always some cors bs 😍🔫

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://portal-frontend-chi.vercel.app/", // Connects to frontend
+    origin: "https://portal-frontend-chi.vercel.app", // Connects to frontend
     credentials: true,
   })
 );
